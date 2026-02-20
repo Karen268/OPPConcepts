acab33a6436e7420f52220cc10b3afb685146e31
KAREN TATIANA MARTINEZ CARDENAS <karenmartinez1157957@correo.itm.edu.co>
2026-02-20 12:46:52 -0500

    OPPConcepts

---
+++ OPPConcepts.Backend/Date.cs
@@ -0,0 +1,40 @@
+﻿namespace OPPConcepts.Backend; 
+
+public class Date
+{
+    private int _year;
+    private int _month;
+    private int _day;
+
+    public Date()
+    {
+        _year = 1900;
+        _month = 1;
+        _day = 1;
+    }
+
+    public Date(int year, int month, int day)
+    {
+        _year = year;
+        _month =month;
+        _day=day;
+    }
+
+    public int Year 
+    {
+        get => _year;
+        set => _year = value;
+    }
+    public int Month
+    {
+        get => _month;
+        set => _month = value;
+    }
+    public int Day 
+    {
+        get => _day;
+        set => _day = value;
+    } 
+
+
+}
