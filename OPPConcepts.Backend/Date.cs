07bdc4fa38518836a8af0d97946d9d117a0db3e0
KAREN TATIANA MARTINEZ CARDENAS <karenmartinez1157957@correo.itm.edu.co>
2026-02-21 20:58:57 -0500

    OPPConcepts

--- OPPConcepts.Backend/Date.cs
+++ OPPConcepts.Backend/Date.cs
@@ -1,4 +1,4 @@
-﻿namespace OPPConcepts.Backend; 
+﻿namespace OPPConcepts.Backend;
 
 public class Date
 {
@@ -15,26 +15,70 @@ public class Date
 
     public Date(int year, int month, int day)
     {
-        _year = year;
-        _month =month;
-        _day=day;
+        Year = year;
+        Month = month;
+        Day = day;
     }
 
-    public int Year 
+    public int Year
     {
         get => _year;
-        set => _year = value;
+        set => _year = ValidateYear(value);
     }
     public int Month
     {
         get => _month;
-        set => _month = value;
+        set => _month = ValidateMonth(value);
     }
-    public int Day 
+    public int Day
     {
         get => _day;
-        set => _day = value;
-    } 
+        set => _day = ValidateDay(value);
+    }
 
+    public override string ToString()
+    {
+        return $"{Year:0000}/{Month:00}/{Day:00}";
+    }
+    private int ValidateYear(int year)
+    {
+        if (year < 0 || year > 2100)
+        {
+            throw new ArgumentOutOfRangeException(nameof(year), "Year must be grathest than 0. ");
+        }
+        return year;
+    }
+    private int ValidateMonth(int month)
+    {
+        if (month < 1 || month > 12)
+        {
+            throw new ArgumentOutOfRangeException(nameof(month), "Month must be beetwen 1 and 12.");
+        }
+        return month;
+    }
+    private int ValidateDay(int day)
+    {
+        if (day < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(day), "Month must be beetwen greathest than 0.");
+        }
+        if (day == 29 && Month == 2 && IsLeapYear(Year))
+        {
+            return day;
+        }
 
+        int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (day > daysInMonth[Month - 1])
+        {
+            throw new ArgumentOutOfRangeException(nameof(day), $"Day must be beetwen 1 and {daysInMonth[Month - 1]} for mont {Month}.");
+        }
+    return day;
+    }
+
+    private bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
 }
+
+
